402d88ef8c41a7581430599eb22b40059e4021c2
taha93230 <71967160+taha93230@users.noreply.github.com>
2021-08-22 23:44:23 +0300

    Delete Temel Python Objeleri ve Veri Yapıları directory

--- Temel Python Objeleri ve Veri Yapıları/Kodlama_Egzersizleri/Kok_Bulma/kok_bulma.py
+++
@@ -1,3 +0,0 @@
-a = float(input("Bir sayı girin : "))
-kok = a**(1/2)
-print("Girilen sayının karekökü = " + str(kok))
--- Temel Python Objeleri ve Veri Yapıları/Odevler/problem2/prob2.py
+++
@@ -1,5 +0,0 @@
-boy = float(input("Boyunuzu metre cinsinden girin ( örneğin 1.89)"))
-kilo = float(input("Kilonuzu kg cinsinden girin ( örneğin 90)"))
-ind = kilo/(boy*boy)
-print("{} boyunda olan ve {} kg ağırlığındaki birinin beden kitle indeksi {} olur.".format(boy,kilo,ind))
-#format olmadan, print ile yazdırma işleminde içerisindeki değişkenleri string veri tipinde istiyor
