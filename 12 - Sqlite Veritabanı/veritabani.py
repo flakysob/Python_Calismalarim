77b8009138e86ae59f22477cecca58f864b0c4b9
Taha <71967160+taha93230@users.noreply.github.com>
2021-09-15 15:49:54 +0300

    Add files via upload

--- 12 - Sqlite Veritabanı/veritabani.py
+++ 12 - Sqlite Veritabanı/veritabani.py
@@ -3,23 +3,28 @@ con = sqlite3.connect("kutuphane.db")
 cursor = con.cursor()
 
 def tabloolustur(): #veritabanı tablo oluşturma
-    cursor.execute("create table if not exists kitaplik(isim text, yazar text, yayinevi text, sayfa_sayisi int)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS kitaplik(isim text, yazar text, yayinevi text, sayfa_sayisi int)")
     con.commit() #işlemin taahhüt edilmesine yarar.
 
 def deger_ekle(isim,yazar,yayinevi,sayfa_sayisi):
-    cursor.execute("insert into kitaplik values(?,?,?,?)",(isim,yazar,yayinevi,sayfa_sayisi))
+    cursor.execute("INSERT INTO kitaplik VALUES(?,?,?,?)",(isim,yazar,yayinevi,sayfa_sayisi))
     con.commit()
 
 def verileri_al():
-    cursor.execute("select * from kitaplik")
+    cursor.execute("SELECT * FROM kitaplik")
     data = cursor.fetchall()
     for i in data:
         print(i)
 
-def veriguncelle(yayine):
-    cursor.execute("update kitaplik set yayinevi=? where yayinevi=?",("x",yayine))
+def veriguncelle(eski_yayinevi,yeni_yayinevi):
+    cursor.execute("UPDATE kitaplik SET yayinevi=? WHERE yayinevi=?",(yeni_yayinevi,eski_yayinevi))
+    con.commit() #veritabanı güncellemesi gerektiğinde kullanılır.
+    print("Başarıyla güncellendi.")
+    
+def verilerisil(ad):
+    cursor.execute("DELETE FROM kitaplik WHERE isim=?",(ad,))
     con.commit()
+    print(ad ," isimli kayıt başarı ile silindi.")
 
-veriguncelle("Deneme")
-
+verilerisil("SucveCeza")
 con.close()
